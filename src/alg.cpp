6dd99b6f880965267c4e8f6d8fdad0b014d20af3
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-07 14:13:28 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,31 +16,35 @@ int getPrior(char s) {
 std::string infx2pstfx(std::string inf) {
     std::string str = "";
     int pr = 0;
-   TStack<char, 100> myStack;
+    TStack<char, 100> myStack;
     for (int i = 0; i < inf.size(); i++) {
         if (inf[i] >= '0' && inf[i] <= '9') {
             if (i < inf.size() - 1 && inf[i + 1] >= '0' && inf[i + 1] <= '9') {
                 str.push_back(static_cast<char>(str[i])));
-            } else {
+            }
+            else {
                 str.push_back(static_cast<char>(inf[i]));
                 str.push_back(' ');
             }
-        } else {
+        }
+        else {
             pr = getPrior(inf[i]);
-            if ((pr == 0) || (myStack.isEmpty()) || (pr > getPrior(myStack.get()))) {
+            if ((pr == 0) || (myStack.isEmpty()) || (pr > getPrior(myStack.top()))) {
                 myStack.push(inf[i]);
-            } else if (pr == 1) {
-                while (myStack.get() != '(') {
-                    if (myStack.get() != ')' && myStack.get() != '(') {
-                        str.push_back(myStack.get());
+            }
+            else if (pr == 1) {
+                while (myStack.top() != '(') {
+                    if (myStack.top() != ')' && myStack.top() != '(') {
+                        str.push_back(myStack.top());
                         str.push_back(' ');
                     }
                     myStack.pop();
                 }
                 myStack.pop();
-            } else {
-                while (!myStack.isEmpty() && getPrior(myStack.get()) >= pr) {
-                    str.push_back(myStack.get());
+            }
+            else {
+                while (!myStack.isEmpty() && getPrior(myStack.top()) >= pr) {
+                    str.push_back(myStack.top());
                     str.push_back(' ');
                     myStack.pop();
                 }
@@ -49,7 +53,7 @@ std::string infx2pstfx(std::string inf) {
         }
     }
     while (!myStack.isEmpty()) {
-        str.push_back(myStack.get());
+        str.push_back(myStack.top());
         str.push_back(' ');
         myStack.pop();
     }
@@ -61,15 +65,17 @@ int eval(std::string post) {
     for (int i = 0; i < post.size(); i++) {
         if (post[i] >= '0' && post[i] <= '9') {
             num = num * 10 + (post[i] - '0');
-        } else if (post[i] == ' ') {
+        }
+        else if (post[i] == ' ') {
             if (num != 0) {
                 stack1.push(num);
                 num = 0;
             }
-        } else {
-            int op1 = stack1.get();
+        }
+        else {
+            int op1 = stack1.top();
             stack1.pop();
-            int op2 = stack1.get();
+            int op2 = stack1.top();
             stack1.pop();
             switch (post[i]) {
             case '+':
@@ -87,5 +93,5 @@ int eval(std::string post) {
             }
         }
     }
-    return stack1.get();
+    return stack1.top();
 }
