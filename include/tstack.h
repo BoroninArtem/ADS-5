b28f6914bea8b395bb8ebd00892add7a4a430fce
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-07 14:18:31 +0300

    b53

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,29 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    static const int size;
+    type arr[size];
+    int top;
+public:
+    TStack() : top(-1) {}
+    type get() const {
+        return arr[top];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
+    void pop() {
+        if (!isEmpty())
+            --top;
+    }
+    void push(type item) {
+        if (!isFull())
+            arr[++top] = item;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
